9b4cb0f0461208485098c1a7e176a8b772ad55c6
MAVERICK-VF142 <ashutosh.jena.mav.1@gmail.com>
2024-06-15 11:42:23 +0530

    added the about me section and skills section

--- script.js
+++ script.js
@@ -21,16 +21,16 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[0].classList.add("AutoCAD");
+        habilidades[1].classList.add("SolidWorks");
+        habilidades[2].classList.add("DraftingDesigning");
+        habilidades[3].classList.add("ManufacturingProcesses");
+        habilidades[4].classList.add("AutomotiveSystems");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[7].classList.add("Creativite");
+        habilidades[8].classList.add("Leadership");
+        habilidades[9].classList.add("ProblemSolving");
     }
 }
 
